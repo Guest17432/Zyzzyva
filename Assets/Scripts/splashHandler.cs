a9366af107ce2fc94a976ca4b9685611415c64e1
GreenWyvern <gamevingw@gmail.com>
2017-09-30 12:00:47 -0400

    Added Water Tile Splashing

---
+++ Assets/Scripts/splashHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splashHandler : MonoBehaviour {
+
+    public float time;
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0);
+        Destroy(this.gameObject, time);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/waterSploosh.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterSploosh : MonoBehaviour {
+
+    public GameObject sploosh;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(sploosh, this.transform.position, this.transform.rotation);
+    }
+}
