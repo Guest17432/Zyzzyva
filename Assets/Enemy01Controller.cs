eaeb28688ed954ef02077a24ae27e84baaa7d426
grcg <gerardo.carmona@cabforward.com>
2017-10-07 03:10:12 -0400

    Enemy sprite.

---
+++ Assets/Enemy01Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class BulletPrefab
+{
+}
+public class Enemy01Controller : MonoBehaviour {
+    public BulletPrefab bulletPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
